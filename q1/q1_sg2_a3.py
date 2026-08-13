e0eb7dd07c020159c307c2b96a98e0b523c28e5d
Raiden-Yumul-2030 <rlyumul2030@clc.pshs.edu.ph>
2026-08-13 09:19:15 +0800

    Add zodiac sign calculation based on birth year
    draft 1

--- q1/q1_sg2_a3.py
+++ q1/q1_sg2_a3.py
@@ -1,1 +1,9 @@
+birthyear = int(input("Hello! Input your birth year to know your zodiac sign. (It cannot be before 1900):"))
+signs = [Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Goat, Monkey, Rooster, Dog, Pig]
 
+if birthyear < 1900:
+  print("That year is not supported (Before 1900)")
+  break
+else:
+  number = (birthyear - 1900) % 12
+  print(f"Your zodiac sign is {signs[number]}!")
