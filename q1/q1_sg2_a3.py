0521540e9caf173a558015ca096efd273888dd56
Raiden-Yumul-2030 <rlyumul2030@clc.pshs.edu.ph>
2026-08-13 09:20:03 +0800

    Fix syntax error in zodiac sign calculation
    Remove break statement to prevent syntax error.

--- q1/q1_sg2_a3.py
+++ q1/q1_sg2_a3.py
@@ -3,7 +3,6 @@ signs = [Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Goat, Monkey, Rooster, Do
 
 if birthyear < 1900:
   print("That year is not supported (Before 1900)")
-  break
 else:
   number = (birthyear - 1900) % 12
   print(f"Your zodiac sign is {signs[number]}!")
