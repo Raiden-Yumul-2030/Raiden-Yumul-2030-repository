e23f8f946429ef4649c137c665449fb2a56e09f4
Raiden-Yumul-2030 <rlyumul2030@clc.pshs.edu.ph>
2026-08-14 20:02:11 +0800

    Implement delay before program exit
    Added a delay before program termination to enhance user experience.

--- q1/q1_sg2_a3.py
+++ q1/q1_sg2_a3.py
@@ -1,3 +1,4 @@
+import time
 birthyear = int(input("Input your birth year to know your zodiac sign. (It cannot be before 1900): ")) #Birthyear input
 signs = ["Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"] #List to be used later
 
@@ -6,5 +7,4 @@ if birthyear < 1900: #Statement to filter years before 1900
 else: #Valid statement function
   number = (birthyear - 1900) % 12 #Uses modulo to loop
   print(f"Your zodiac sign is {signs[number]}!") #Output
-
-input("Type anything to end the program: ") #So that the code can end on the user's terms
+time.sleep(2) #So the user can see their sign before program terminates
